9766b020e27641fc10dac93938af321a80672f22
techtorialdevops <130173463+techtorialdevops@users.noreply.github.com>
2024-05-04 12:09:00 -0500

    output commented out

--- module/main.tf
+++ module/main.tf
@@ -11,7 +11,3 @@ output region {
 output key_name {
     value = module.dev.key_name
 }
-
-output "ip" {
-  value       = aws_instance.terraform6.public_ip
-}
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "ip" {
-  value       = aws_instance.terraform6.public_ip
-}
+# output "ip" {
+#   value       = aws_instance.terraform6.public_ip
+# }
